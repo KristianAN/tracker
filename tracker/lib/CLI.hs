ae9966cfbd0b567eced6f8eeb8be01b5204dd95b
Kristian Nedrevold-Hansen <kristian@krined.no>
2025-03-09 21:27:14 +0100

    Set up CLI parser with start and add commands

--- tracker/lib/CLI.hs
+++ tracker/lib/CLI.hs
@@ -1,16 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module CLI (runCli) where
 
+import Data.Text qualified as T
 import Options.Applicative
+import Project qualified as P
 
-data CLI = Start | Add deriving (Show)
+data CLI = Start {project :: Maybe T.Text} | AddProject P.Project deriving (Show)
+
+startParser :: Parser CLI
+startParser =
+    Start
+        <$> optional
+            ( strOption
+                (long "project" <> short 'p' <> help "Optional project name to override current directory name")
+            )
+
+addProjectParser :: Parser CLI
+addProjectParser =
+    AddProject
+        <$> ( P.Project
+                <$> strOption
+                    ( long "name" <> short 'n' <> help "Name of the project to add"
+                    )
+                <*> optional
+                    ( strOption
+                        (long "externalId" <> short 'e' <> help "optional reference to external system")
+                    )
+            )
 
 cli :: Parser CLI
 cli =
     hsubparser
-        (command "start" (info (pure Start) (progDesc "Start time tracking")))
+        ( command "start" (info startParser (progDesc "Start time tracking"))
+            <> command "add" (info addProjectParser (progDesc "Add a project for tracking"))
+        )
 
 cliInfo :: ParserInfo CLI
-cliInfo = info (cli <**> helper) (fullDesc <> progDesc "Tracker, track your time" <> header "Tracker lets you track the time you spend working on your projects")
+cliInfo = info (cli <**> helper) (fullDesc <> header "Tracker lets you track the time you spend working on your projects")
 
 runCli :: IO CLI
 runCli =
