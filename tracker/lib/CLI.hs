00d812a09772c00ef699f76a1f503257f84a981e
Kristian Nedrevold-Hansen <kristian@krined.no>
2025-03-09 20:27:08 +0100

    Add help text to cli

--- tracker/lib/CLI.hs
+++ tracker/lib/CLI.hs
@@ -10,7 +10,11 @@ cli =
         (command "start" (info (pure Start) (progDesc "Start time tracking")))
 
 cliInfo :: ParserInfo CLI
-cliInfo = info cli (fullDesc <> progDesc "Tracker, track your time" <> header "Tracker lets you track the time you spend working on your projects")
+cliInfo = info (cli <**> helper) (fullDesc <> progDesc "Tracker, track your time" <> header "Tracker lets you track the time you spend working on your projects")
 
 runCli :: IO CLI
-runCli = execParser cliInfo
+runCli =
+    customExecParser p opts
+  where
+    opts = cliInfo
+    p = prefs showHelpOnEmpty
