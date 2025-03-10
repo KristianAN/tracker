ac6aa40bb5610276ae7326a71e6aa525458e3cd1
Kristian Nedrevold-Hansen <kristian@krined.no>
2025-03-10 01:06:28 +0100

    Can now start tracking from CLI

--- tracker/lib/Interpreter.hs
+++ tracker/lib/Interpreter.hs
@@ -1,35 +1,58 @@
 module Interpreter (interpret) where
 
 import CLI
+import Data.Functor
 import Data.Text qualified as T
+import Database.SQLite.Simple
 import Persistence.DatabaseUtils (withTrackerConnection)
 import Persistence.TrackerRepository
-import Project (prettyPrintProject)
+import Project
+import System.Directory (getCurrentDirectory)
+import System.FilePath (takeFileName)
+
+listAllProjects :: Connection -> IO ()
+listAllProjects c = do
+    result <- selectAllProjects c
+    case result of
+        Success projects
+            | not (null projects) ->
+                let prettyPrinted = fmap prettyPrintProject projects
+                    projectStr = T.intercalate (T.pack "\n") prettyPrinted
+                 in putStrLn $ T.unpack projectStr
+            | otherwise ->
+                putStrLn
+                    "Nothing to list, consider adding a project with tracker add"
+        Error reason -> putStrLn $ T.unpack reason
+
+startTracking :: Maybe T.Text -> IO ()
+startTracking project =
+    case project of
+        Just projectName ->
+            withTrackerConnection $ \c -> do
+                result <- insertNewEntry c projectName
+                case result of
+                    Success () -> putStrLn $ "Using provided project name. Started tracking time for project: " <> T.unpack projectName
+                    Error reason -> putStrLn $ T.unpack reason
+        Nothing -> do
+            currentDir <- getCurrentDirectory <&> takeFileName
+            withTrackerConnection $ \c -> do
+                result <- insertNewEntry c $ T.pack currentDir
+                case result of
+                    Success () -> putStrLn $ "Started tracking time for project: " <> currentDir
+                    Error reason -> putStrLn $ T.unpack reason
+
+addProject :: Project -> IO ()
+addProject project =
+    withTrackerConnection $ \c -> do
+        result <- insertProject c project
+        case result of
+            Success () -> putStrLn "Added project"
+            Error reason -> putStrLn $ T.unpack reason
 
 interpret :: IO ()
 interpret = do
     opts <- runCli
     case opts of
-        List ->
-            withTrackerConnection $ \c -> do
-                result <- selectAllProjects c
-                case result of
-                    Success projects
-                        | not (null projects) ->
-                            let prettyPrinted = fmap prettyPrintProject projects
-                                projectStr = T.intercalate (T.pack "\n") prettyPrinted
-                             in putStrLn $ T.unpack projectStr
-                        | otherwise ->
-                            putStrLn
-                                "Nothing to list, consider adding a project with tracker add"
-                    Error reason -> putStrLn $ T.unpack reason
-        Start project ->
-            case project of
-                Just project -> undefined
-                Nothing -> undefined
-        AddProject project ->
-            withTrackerConnection $ \c -> do
-                result <- insertProject c project
-                case result of
-                    Success () -> putStrLn "Added project"
-                    Error reason -> putStrLn $ T.unpack reason
+        List -> withTrackerConnection listAllProjects
+        Start project -> startTracking project
+        AddProject project -> addProject project
